3e5250e253c22acf49a3bb84e6fffa953fcd22d3
carbonhrz <77118268+carbonhrz@users.noreply.github.com>
2021-12-07 13:25:31 +0100

    Task:  Schreibe eine Funktion, die die Zahlen von 1 bis 100 zurückgibt. Manche Zahlen sollen dabei allerdings übersetzt werden [1]:
          Für Vielfache von 3 gibt „Fizz“ aus.
          Für Vielfache von 5 gib „Buzz“ aus.
          Für Vielfache von 3 und 5 gib „FizzBuzz“ aus.
    
          Variation:
          „Fizz“ soll auch zurückgegeben werden, wenn die Zahl eine 3 als Ziffer enthält, z.B. 13. „Buzz“ soll auch zurückgegeben werden, wenn die Zahl eine 5 als Ziffer enthält, z.B. 52.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,13 @@
+
 public class FizzBuzz {
 
     public static void main (String[] args) {
-        for (int i = 1; i <= 100; i++) {
+        FizzBuzz f = new FizzBuzz();
+        f.analyze_digits(100);
+}
+
+    public void analyze_digits (int digit) {
+        for (int i = 1; i <= digit; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println(i + " -> FizzBuzz");
             } else if (i % 5 == 0) {
@@ -9,8 +15,14 @@ public class FizzBuzz {
             } else if (i % 3 == 0) {
                 System.out.println(i + " -> Fizz");
             } else {
-                System.out.println(i);
+                if ((i + "").toString().contains("3")) {
+                    System.out.println(i + "-> Fizz");
+                } else if ((i + "").toString().contains("5")) {
+                    System.out.println(i + "-> Buzz");
+                } else {
+                    System.out.println(i);
+                }
             }
         }
     }
-}
+    }
